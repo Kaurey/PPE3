733fe1bd46dce921648f66885d74e8d3c00661e2
Kaurey <corentinbouet77120@gmail.com>
2021-01-21 14:46:58 +0100

    maj

---
+++ GSB/vues/v_etatFrais.php
@@ -0,0 +1,102 @@
+﻿<div style="text-align: center;">
+<input type="button" value="imprimer" onclick="imprimer();">
+</div>
+<script type="text/javascript">     
+function imprimer() {    
+	var imprimer = document.getElementById('imprimer');
+	var popupcontenu = window.open('', '_blank');
+	popupcontenu.document.open();
+	popupcontenu.document.write('<html><body onload="window.print()">' + imprimer.innerHTML + '</html>');
+	popupcontenu.document.close();
+}
+</script>
+<div id="imprimer">
+  <h3>Fiche de frais du mois <?php echo $numMois."-".$numAnnee?> : <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+    </h3>
+    <div class="encadre">
+      <style type="text/css">@media print{.encadre{text-align: center;}}</style>
+    <p>
+        Etat : <?php echo $libEtat?> depuis le <?php echo $dateModif?> <br> Montant validé : <?php echo $montantValide?>             
+    </p>
+    <div id="espace" class="espasce">
+  	<table class="listeLegere">
+            <style type="text/css">@media print{.listeLegere{text-align: center;margin: 0 auto;border: 1px solid black;border-spacing: 35px 1rem;};}</style>
+  	   <caption>Eléments forfaitisés </caption>
+    <th>Frais forfaitaire</th>
+    <th>Quantité</th>
+    <th>Montant  unitaire</th>
+    <th>Total</th>
+		</tr>
+        <tr>
+        <?php
+        $sommeFinal1=0;
+// nom forfaitaire ligne tableau
+          foreach (  $lesFraisForfait as $unFraisForfait  ) 
+		  {
+				$libelle = $unFraisForfait['libelle'];
+        $prix = $unFraisForfait['montant'];
+        $quantite = $unFraisForfait['quantite'];
+        $somme=$prix*$quantite;
+        $sommeFinal1+=$somme;
+		?>
+                <tr class="qteForfait">
+                  <td><?php echo $libelle?></td>
+                  <td><?php echo $quantite?></td>
+                  <td><?php echo $prix?></td> 
+                  <td><?php echo $somme?></td>
+              </tr>
+		 <?php
+          }
+		?>
+		</tr>
+    </table>
+
+<?//--------------------------------MODIFIER SEULEMENT AU DESSUS-------------------------------------------------------------------------------------------------------//?>
+
+  	<table class="listeLegere">
+  	   <p>Descriptif des éléments hors forfait - <?php echo $nbJustificatifs ?> justificatifs reçus -
+       </p>
+             <tr>
+                <th class="date">Date</th>
+                <th class="libelle">Libellé</th>
+                <th class='montant'>Montant</th>                
+             </tr>
+        <?php     
+        $sommeFinal2=0; 
+          foreach ( $lesFraisHorsForfait as $unFraisHorsForfait ) 
+		  {
+			$date = $unFraisHorsForfait['date'];
+			$libelle = $unFraisHorsForfait['libelle'];
+			$montant = $unFraisHorsForfait['montant'];
+      $sommeFinal2+=$montant;
+		?>
+             <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo $libelle ?></td><?//montant en bas sous date//?>
+                <td><?php echo $montant ?></td>
+             </tr> 
+        <?php 
+          }
+		?>
+    <th>Total Final</th>
+    <td><?php echo $sommeFinal2+$sommeFinal1 ?></td>
+    </table>
+    <p style="text-align: right;margin-right:10%">Signature</p>
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GSB/vues/v_listeMois.php
@@ -0,0 +1,69 @@
+﻿ <div id="contenu">
+      <h2>Mes fiches de frais</h2>
+      <h3>Mois à sélectionner : </h3>
+      <form action="index.php?uc=etatFrais&action=voirEtatFrais" method="post">
+      <div class="corpsForm">
+         
+      <p>
+	 
+        <label for="lstMois" accesskey="n">Mois : </label>
+        <select id="lstMois" name="lstMois">
+            <?php
+			foreach ($lesMois as $unMois)
+			{
+			  $mois = $unMois['mois'];
+				$numAnnee =  $unMois['numAnnee'];
+				$numMois =  $unMois['numMois'];
+				if($mois == $moisASelectionner){
+				?>
+				<option selected value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
+				else{ ?>
+				<option value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
+			
+			}
+           
+		   ?>    
+            
+        </select>
+      </p>
+      </div>
+      <div class="piedForm">
+      <p>
+        <input id="ok" type="submit" value="Valider" size="20" />
+        <input id="annuler" type="reset" value="Effacer" size="20" onclick="window.location.href='index.php?uc=etatFrais&action=selectionnerMois';">
+        	<a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais"></a>
+    	</input>
+      </p> 
+      </div>
+      </form>
+
+
+<!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>
+      $(document).ready(function(){
+        // Set trigger and container variables
+        var trigger = $('button a'),
+            container = $('#content');
+        
+        // Fire on click
+        trigger.on('click', function(){
+          // Set $this for re-use. Set target from data attribute
+          /*var $this = $(this),
+            target = $this.data('target');       
+          */
+          // Load target page into container
+          container.load('index.php');
+          
+          // Stop normal link behavior
+          return false;
+        });
+      });
+    </script>
+        <button><a href="#" data-target="index">Effacer</a></button>
+    <div id="content"></div>
+-->
